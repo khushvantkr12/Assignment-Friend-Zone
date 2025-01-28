32c9316cd6cc4d1a45580a668ed5f3fa90c3013f
khushvant kumar <98275946+khushvantkr12@users.noreply.github.com>
2025-01-28 21:13:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/friends', require('./routes/friends'));
 
+app.get("/",(req,res)=>{
+    res.send("heyy your server is running successfully");
+    
+ })
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
